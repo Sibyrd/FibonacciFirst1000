51d8330163aff0241aa0f1571884aa3a13397f48
Sibyrd <mail@theobird.com>
2024-10-02 16:43:01 +0100

    cleaned up

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-Fn = []
+Fn = [0, 1]
 
 inp = int(input("Enter how many digits you would like to find the first value of n for the respective term in the Fibonacci sequence to consist of that many digits: "))
 print(0)
+
+
 def fib(n):
-    if len(Fn) == 0:
-        Fn.append(0)
-        Fn.append(1)
-        Fn.append(1)
-        print(1)
-        print(1)
-        return
-    NewFn = (Fn[n - 1]) + (Fn[n - 2])
-    Fn.append(NewFn)
-    print(NewFn)
+    if len(Fn) == 2:    print(1)
+    new_fn = (Fn[n - 1]) + (Fn[n - 2])
+    Fn.append(new_fn)
+    print(new_fn)
+
 
 def check():
     try:
@@ -21,7 +18,8 @@ def check():
     except:
         return False
 
+
 while check() or len(Fn) == 0:
     fib(len(Fn))
 
-print("The first value of n in which the term contains " + str(inp) +  " digits is " + str(len(Fn)-1))
+print("The first value of n in which the term contains " + str(inp) + " digits is " + str(len(Fn) - 1))
